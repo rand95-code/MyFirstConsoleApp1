4b4c5d36ef60d5e3e16b0e70b7741e44b8cbaa7f
Rand Farhan <rand.farhan@gmail.com>
2019-10-21 16:04:41 +0200

    first

---
+++ MyFirstConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyFirstConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Write("Welcome to Lexicon");
+
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
